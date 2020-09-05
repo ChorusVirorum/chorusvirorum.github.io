598c8c8bdccf351bfcfe5849eff22603426459b6
Joakim Borgström <allmecka@gmail.com>
2020-09-05 17:18:05 +0200

    Starting on vue page

---
+++ src/components/Ribbon.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="CvRibbon" :class="modifiers">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CvRibbon',
+  computed: {
+    modifiers() {
+      return {
+      };
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.CvRibbon {
+  height: .25rem;
+  background-color: white;
+  border-top: .5rem solid var(--color-cv);
+  border-bottom: .5rem solid black;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router';
+
+import App from './App.vue'
+import Home from './pages/Home.vue'
+import About from './pages/About.vue'
+import Join from './pages/Join.vue'
+
+Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { 
+      path: '/', 
+      name: 'home', 
+      component: Home, 
+      meta: {
+        title: 'Startsidan'
+      } 
+    },
+    { 
+      path: '/om-oss', 
+      name: 'about', 
+      component: About, 
+      meta: {
+        title: 'Om oss'
+      } 
+    },
+    { 
+      path: '/ga-med', 
+      name: 'joint', 
+      component: Join, 
+      meta: {
+        title: 'Gå med i kören'
+      } 
+    },
+  ]
+});
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
